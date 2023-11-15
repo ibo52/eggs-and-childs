27ae7871c0a28482fb6bc5acac716af6163e7d69
ibo52 <halilibrahimmut@hotmail.com>
2023-11-15 12:56:15 +0300

    Review: Move send and receive functions from socket-util.c to Socket.c; add virtual functions table

--- socket/include/Socket.h
+++ socket/include/Socket.h
@@ -4,29 +4,53 @@
 #include<stdint.h>
 #include "socket-util.h"
 typedef struct __dataBuffer dataBuffer;//forward declaration to socket-util.h
-
+typedef struct __Socket_VTable Socket_VTable;
 /*
 *	CommunicationObject
 *	struct holds socket info and buffers for communication
 */
+
 typedef struct __Socket{
 	int fd;						//file descriptor
 	struct sockaddr_in socket;	//sockaddr struct
 	
 	dataBuffer *recv_buff;		//buffer to receive message
 	dataBuffer *send_buff;		//buffer to send message
+	
+	const Socket_VTable* vtable; //virtual functions table
 }Socket;
-/*
-typedef struct __Server_VTable Server_VTable;
 
-struct __Server_VTable{
-	Server* (*new)(int addr, int type);
-	void (*destroy)(Server** self);
+struct __Socket_VTable{
+	dataBuffer (*receive)(Socket* self);
+	dataBuffer (*send)(Socket* self);
 };
+
+/*
+*	Create new object (Allocate data on memory)
 */
 Socket* socket__new(int address, int port, int type);
 
+/*
+*	Destroy the object(free the address that memory points)
+*/
 void socket__destroy(Socket** self);
+
+/*
+*	Send message through Socket object. Common interface for TCP and UDP sockets
+*	
+*	@param self	: destination Socket to forward message
+*	@return		: Bytes sent in total
+*/
+dataBuffer socket__send(Socket* self);
+
+
+/*
+*	Receive message through Socket object. Common interface for TCP and UDP sockets
+*	
+*	@param self: Socket to listen for a message receivation from others
+*	@return	   : Bytes received in total
+*/
+dataBuffer socket__receive(Socket* self);
 /*
 void server__start(Server* self);
 
@@ -34,4 +58,11 @@ static const Server_VTable vtable_server;
 vtable_server->start=&server__start;
 vtable_server->join=&server__join;
 */
+
+//Default Virtual functions table
+static const Socket_VTable Socket_VTable__default={
+	.receive=socket__receive,
+	.send=socket__send
+};
+
 #endif
--- socket/src/Socket.c
+++ socket/src/Socket.c
@@ -77,6 +77,10 @@ Socket* socket__new(int addr, int port, int type){
 		    exit(errno);
 		}
 	}
+	
+	
+	//bind virtual function table contents
+	self->vtable=&Socket_VTable__default;
 	return self;
 }
 
@@ -89,4 +93,70 @@ void socket__destroy(Socket** self){
 	free( (*self) );
 	(*self)=NULL;
 	printf("Socket object destructed\n");
+}
+
+static int min(int a, int b){
+	if(a<b)
+		return a;
+		
+	return b;
+}
+
+dataBuffer socket__send(Socket* self){
+	/*
+	@param self	: sender Socket to forward message to other
+	@return		: Bytes sent in total
+	*/
+	int MAX_BYTES_PER_SEND=65536-28;//28 allocated for is udp protocol headers
+	int sent_size=0;
+	int length=self->send_buff->size;
+
+	struct sockaddr_in client=self->socket;	//recvfrom will fill this struct destination socket
+	socklen_t client_len=sizeof(client);
+	
+	while( sent_size < length ){
+	
+		int size;
+		int per_send_len=min(MAX_BYTES_PER_SEND, length-sent_size);
+		
+		if( (size = sendto(self->fd, self->send_buff->buffer + sent_size, per_send_len, 0, (struct sockaddr *)&client, client_len)) <0 ){
+			fprintf(stderr, "Error while sending to socket(Totally %i bytes sent): %s\n",sent_size, strerror(errno));
+			exit(errno);
+		}
+		sent_size+=size;
+	}//will break safely if equal to 0; will exit if<0
+	//printf("sent %i bytes of %s\n",sent_size, (char*)self->send_buff->buffer);
+	self->send_buff->size=sent_size;
+	dataBuffer retData={NULL, sent_size, 0};
+	return retData;
+}
+
+dataBuffer socket__receive(Socket* self){
+	/*
+	@param self: Socket to listen for a message receivation from others
+	@return	   : Bytes received in total
+	*/
+	
+		int recv_retval;			//total received size of message from procedure call 
+		struct sockaddr_in *client=malloc(1*sizeof(struct sockaddr_in));	//recvfrom will fill this struct
+		socklen_t client_address_size = sizeof(*client);
+			
+		if( (recv_retval= recvfrom(self->fd, self->recv_buff->buffer , self->recv_buff->max_size, 0, (struct sockaddr *)client, &client_address_size)) <0 ){
+			perror("Error while receiving from socket");
+			exit(recv_retval);
+		
+		}
+		
+	self->recv_buff->size=recv_retval;
+	
+	dataBuffer retData={client, recv_retval, 0};
+	/*
+	int addr=client->sin_addr.s_addr;
+	
+	
+	printf("Receive SOCKET func:  Domain:%s address:%i.%i.%i.%i\n",(client->sin_family == AF_INET?"AF_INET":"UNKNOWN"),
+	(addr&0xff), (addr>>8&0xff), (addr>>16&0xff), addr>>24&0xff);	
+	*/
+	//Next implementation: If socket type==TCP; then do not malloc struct sockaddr_in *client, return NULL
+	return retData;
 }
