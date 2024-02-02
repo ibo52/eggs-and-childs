7dff98b4b3da874599d3ed2e8c60342aa36c2732
ibo52 <halilibrahimmut@hotmail.com>
2024-02-02 10:55:02 +0300

    add test to check packet re-ordering functionality

---
+++ socket/test/PacketMonitor/test1.c
@@ -0,0 +1,24 @@
+#include "PacketMonitor.h"
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char** argv){
+
+    PacketMonitor* p=packet_monitor__new();
+    
+    int seq[10]={ 3,2,1,4,7,5,9,6,8, 10};
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%i'th arrived: %i\n---------------------------------------\n",i+1, seq[i]);
+        
+        RTP* rtp=rtp__new();
+        rtp->sequence_number=seq[i];
+        
+        rearrange_incoming_rtp_packet(p->store, rtp );
+        printf("---------------------------------------\n");
+    }
+    
+    packet_monitor__destroy(&p);
+}
